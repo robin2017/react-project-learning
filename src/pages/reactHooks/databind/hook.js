b241c819fd986af44ba7a35d41a933ed7aa18f97
robin2017 <robin.seu@foxmail.com>
2020-08-15 12:25:32 +0800

    tt

--- src/pages/reactHooks/databind/hook.js
+++ src/pages/reactHooks/databind/hook.js
@@ -1,11 +1,11 @@
 //1、此处不需要引入React，因为没有渲染JSX
-import {useState} from 'react'
-export const useBind = (initialValue,_onChange) => {
-    const [value, setValue] = useState(initialValue || "");
+import { useState } from 'react'
+export const useBind = (initialValue, _onChange) => {
+    const [value, setValue] = useState(initialValue)
     const onChange = e => {
-        setValue(e.target.value);
-        _onChange&&_onChange(e.target.value)
-    };
-    return { value, onChange };
+        setValue(e.target.value)
+        _onChange && _onChange(e.target.value)
+    }
+    return { value, onChange }
 }
 
