8fe6ba3a40e23b5d3ae9d46bcafeb2caa3cb4d5d
robin2017 <robin.seu@foxmail.com>
2020-08-15 16:28:30 +0800

    td

--- src/pages/reactHooks/hoc2hook/index3.jsx
+++ src/pages/reactHooks/hoc2hook/index3.jsx
@@ -6,7 +6,7 @@ import { CompHoc } from "./utils";
  * 3、数据在远程，将获取数据逻辑封装到hook中，更好的抽象
  */
 export default () => {
-  const TablePanel = CompHoc(Table, "dataSource");
+  const TablePanel = CompHoc(Table, "dataSource", []);
   return (
     <TablePanel hook={useData} hookParam={{ year: 2020 }}>
       <Table.Column title="Id" dataIndex="id" />
--- src/pages/reactHooks/hoc2hook/utils.js
+++ src/pages/reactHooks/hoc2hook/utils.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {Loading} from '@alifd/next'
+import { Loading } from '@alifd/next'
 //这个是通用的封装，将dataAttr和hook,hookParam放在不同层级
-export const CompHoc = (Comp, dataAttr) => {
+export const CompHoc = (Comp, dataAttr, dataInit) => {
     return ({ hook, hookParam, ...props }) => {
         const data = hook(hookParam);
         // 这里必须添加...props，否则渲染失败
         const newProps = {
-            [dataAttr]: data||[],
+            [dataAttr]: data || dataInit || [],
             ...props,
         };
-        return <Loading visible={data===null} style={{width:'100%',height:'100%'}}>
-            {/* 没拿到数据就不渲染组件(这个策略不好！！！) */}
+        return <Loading visible={data === null} style={{ width: '100%', height: '100%' }}>
+            {/* 没拿到数据也要渲染，但数据要填充) */}
             <Comp {...newProps} />
         </Loading>;
     };
