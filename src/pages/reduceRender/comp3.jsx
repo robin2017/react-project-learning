115033c8192a46e19e34d66e70667d75f3a23a66
bota.lb <bota.lb@alibaba-inc.com>
2020-08-15 21:18:24 +0800

    usememo

---
+++ src/pages/reduceRender/comp3.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+export default ({ id }) => {
+    console.log('comp3渲染')
+    return (
+        <div>hello,comp{id}</div>
+    )
+}
--- src/pages/reduceRender/index.jsx
+++ src/pages/reduceRender/index.jsx
@@ -1,18 +1,24 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useState, useMemo } from 'react'
 import Comp1 from './comp1'
 import Comp2 from './comp2'
+import Comp3 from './comp3'
 export default () => {
     const [count, setCount] = useState(0)
     const btnClick = () => {
         setCount(count + 1)
     }
     console.log('父组件渲染')
+    let id3 = 3
     return (
         <div>
             <div>{count}</div>
             <button onClick={btnClick}>增加</button>
-            <Comp1 id={1}/>
-            <Comp2 id={2}/>
+            <Comp1 id={1} />
+            <Comp2 id={2} />
+            {
+                // 在组件调用的时候，包裹一层useMemo，但是不优雅
+                useMemo(() => <Comp3 id={id3} />, [id3])
+            }
         </div>
     )
 }
