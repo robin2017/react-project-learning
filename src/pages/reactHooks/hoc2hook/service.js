81036a9c992218009ea609956d680bd23e592be9
robin2017 <robin.seu@foxmail.com>
2020-08-15 16:00:30 +0800

    没数据的假象

--- src/pages/reactHooks/hoc2hook/service.js
+++ src/pages/reactHooks/hoc2hook/service.js
@@ -1,11 +1,15 @@
 const queryAjax = (params = {}) => {
-    return Promise.resolve({
-        data: [
-            { id: 1, time: "2016" },
-            { id: 2, time: "2017" },
-            { id: 3, time: params.year },
-        ],
-    });
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                data: [
+                    { id: 1, time: "2016" },
+                    { id: 2, time: "2017" },
+                    { id: 3, time: params.year },
+                ],
+            })
+        }, 3000);
+    })
 };
 export default {
     queryAjax
