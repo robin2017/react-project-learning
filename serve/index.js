8da37e6381132f42cc39041dae395e1e4e693567
bota.lb <bota.lb@alibaba-inc.com>
2020-09-26 22:15:40 +0800

    http

--- serve/index.js
+++ serve/index.js
@@ -1,13 +1,21 @@
 const path = require('path')
+// const fs = require('fs')
 const koa = require('koa')
 const static = require('koa-static')
 const router = require('koa-router')
+// const https = require('https')
+// const sslify = require('koa-sslify').default;//http强制HTTPS
 const staticPath = './public'
 const PORT = 12808;
 const persons = [{ name: 'robin' }, { name: 'bota' }]
 const app = new koa()
+// app.use(sslify())
 const myRouter = new router()
 app.use(static(path.join(__dirname, staticPath)))
+// var options = {
+//     key: fs.readFileSync('./private_key.pem'),  //私钥文件路径
+//     cert: fs.readFileSync('./ca-cert.pem')  //证书文件路径
+// };
 myRouter.get('/', async (ctx) => {
     ctx.body = 'hello,koa'
 })
@@ -34,3 +42,6 @@ app.use(myRouter.routes()).use(myRouter.allowedMethods());
 app.listen(PORT, () => {
     console.log('koa服务器开启，地址为:http://localhost:' + PORT)
 })
+// https.createServer(options,app.callback()).listen(PORT,()=>{
+//     console.log('koa服务器开启，地址为:http://localhost:' + PORT)
+// })
